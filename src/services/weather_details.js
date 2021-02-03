a3a7a4d558561b6d37ef3598dfdbbddc7048b223
dhiraj kumar <dhirajpallavipriya@Unknown.gateway>
2021-02-03 11:13:27 +1100

    minor clean up. remove test api call

--- src/services/weather_details.js
+++ src/services/weather_details.js
@@ -1,13 +1,7 @@
 import data from "./testData.json";
 import axios from "axios";
 
-const testFun = async () => {
-  return new Promise((resolve, reject) => resolve(data));
-};
 export const fetchData = async (location) => {
-  // const data =  await testFun();
-  // console.log(data)
-  // return data
   return await axios.get(
     `https://ws.weatherzone.com.au/?lt=aploc&lc=${location}&locdet=1&latlon=1&pdf=twc(period=48,detail=2)&u=1&format=json`
   );
